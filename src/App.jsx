12a3dce81f2169f7bfb9e2ac858faff263d73a03
naveeeennex-cpu <naveeeen.nex@gmail.com>
2026-05-07 15:04:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Features from './components/Features'
-import Stats from './components/Stats'
-import Services from './components/Services'
-import Portfolio from './components/Portfolio'
-import HowItWorks from './components/HowItWorks'
-import Clients from './components/Clients'
-import Footer from './components/Footer'
-import WhatsAppButton from './components/WhatsAppButton'
+import Home from './pages/Home'
+import ProductPage from './pages/ProductPage'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-dark">
-      <Toaster position="top-right" />
-      <Navbar />
-      <Hero />
-      <Features />
-      <Stats />
-      <Services />
-      <Portfolio />
-      <HowItWorks />
-      <Clients />
-      <Footer />
-      <WhatsAppButton />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-dark">
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:slug" element={<ProductPage />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
