4fc1718b2d242523e4fda005df058d1467274c6c
naveeeennex-cpu <naveeeen.nex@gmail.com>
2026-05-07 15:23:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Home from './pages/Home'
 import ProductPage from './pages/ProductPage'
+import WorkPage from './pages/WorkPage'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products/:slug" element={<ProductPage />} />
+          <Route path="/work/:slug" element={<WorkPage />} />
           <Route path="*" element={<Home />} />
         </Routes>
       </div>
