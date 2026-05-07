0dedc685a91d88d41a36e60b64f76238921342e8
naveeeennex-cpu <naveeeen.nex@gmail.com>
2026-05-07 15:01:36 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import Stats from '../components/Stats'
+import Services from '../components/Services'
+import OurProducts from '../components/OurProducts'
+import HowItWorks from '../components/HowItWorks'
+import Footer from '../components/Footer'
+import WhatsAppButton from '../components/WhatsAppButton'
+
+export default function Home() {
+  const { hash } = useLocation()
+
+  useEffect(() => {
+    if (!hash) return
+    const id = hash.replace('#', '')
+    const el = document.getElementById(id)
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    }
+  }, [hash])
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Stats />
+      <Services />
+      <OurProducts />
+      <HowItWorks />
+      <Footer />
+      <WhatsAppButton />
+    </>
+  )
+}
